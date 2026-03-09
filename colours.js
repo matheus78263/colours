e7406154d4e193af074d41e572524af027b078bf
matheus78263 <78263@student.dorset-college.ie>
2026-03-09 10:53:29 +0000

    colours button configured

--- colours.js
+++ colours.js
@@ -1,20 +1,18 @@
-const btnColour = document.getElementById('change-color');
-const boxDisplay = document.getElementById('bg-color');
-const bigSection = document.getElementById('section');
+     const btnColor = document.getElementById('change-color');
+        const bgColor = document.getElementById('bg-color');
+        const bigSection = document.querySelector('section');
 
-btnColour.addEventListener('click', () => {
-    let strHexColour = '#';
-    const arrHex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F']
-    for (let i = 0; i < 6; i++){
-        strHexColour += arrHex[getRandomNumber(arrHex.length)];
-    }
-    boxDisplay.innerText = strHexColour;
-    bigSection.style.backgroundColor = strHexColour;
+        btnColor.addEventListener('click', () => {
+            let strHexColor = '#';
+            const arrayHex = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
+            for (let i = 0; i < 6; i++) {
+                strHexColor += arrayHex[Math.floor(Math.random() * 16)];
+            }
+            bigSection.style.backgroundColor = strHexColor;
+            bgColor.textContent = strHexColor;
+        });
 
-});
-
-
-// Generate random number in range from 0 to max-1
+          // Generate random number in range from 0 to max-1
     function getRandomNumber(max) {
-        return Math.floor(Math.random() * max);
-    };
+    return Math.floor(Math.random() * max);
+}
