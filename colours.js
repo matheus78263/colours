886cd94c97d485ce2ed2250aa2bc6b21545cebcd
matheus78263 <78263@student.dorset-college.ie>
2026-03-09 10:39:24 +0000

    function added

--- colours.js
+++ colours.js
@@ -1,4 +1,20 @@
+const btnColour = document.getElementById('change-color');
+const boxDisplay = document.getElementById('bg-color');
+const bigSection = document.getElementById('section');
+
+btnColour.addEventListener('click', () => {
+    let strHexColour = '#';
+    const arrHex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F']
+    for (let i = 0; i < 6; i++){
+        strHexColour += arrHex[getRandomNumber(arrHex.length)];
+    }
+    boxDisplay.innerText = strHexColour;
+    bigSection.style.backgroundColor = strHexColour;
+
+});
+
+
 // Generate random number in range from 0 to max-1
     function getRandomNumber(max) {
-    return Math.floor(Math.random() * max);
-    
+        return Math.floor(Math.random() * max);
+    };
